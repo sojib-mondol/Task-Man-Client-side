b139c38d899afb0308c5356dbd2ad6176dac7977
sojib-sudo <sojibmandol@gmail.com>
2022-12-28 13:18:45 +0600

    add task form design done

---
+++ src/Pages/MyTask/MyTask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyTask = () => {
+    return (
+        <div className='mt-44'>
+            <h2>Here is my task plsease compleate those</h2>
+            <h2>Here is my task plsease compleate those</h2>
+        </div>
+    );
+};
+
+export default MyTask;
