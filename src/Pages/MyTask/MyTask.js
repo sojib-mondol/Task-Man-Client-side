b1b37a4f3bd4204bc0b1bc7e74514b20bed91963
mohiuddin.ice.ru <mohiuddin.ice.ru@gmail.com>
2022-12-29 12:03:10 +0600

    modal added

--- src/Pages/MyTask/MyTask.js
+++ src/Pages/MyTask/MyTask.js
@@ -1,42 +1,26 @@
-import { useQuery } from '@tanstack/react-query';
-import { data } from 'autoprefixer';
-import React, { useState } from 'react';
-import { toast } from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
-import ConfirmationModal from './ConfirmationModal';
-import TaskCard from './TaskCard';
+import { useQuery } from "@tanstack/react-query";
+import { data } from "autoprefixer";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import ConfirmationModal from "./ConfirmationModal";
+import TaskCard from "./TaskCard";
 
 const MyTask = () => {
-    const [deleting, setDeleting] = useState(null);
-    
-    
     const navigate = useNavigate();
-    const {data: tasks = [], refetch, isLoading} = useQuery({
-        queryKey: ['tasks'],
-        queryFn: async() =>{
-            const res = await fetch('http://localhost:5000/tasks');
+
+    const {
+        data: tasks = [],
+        refetch,
+        isLoading,
+    } = useQuery({
+        queryKey: ["tasks"],
+        queryFn: async () => {
+            const res = await fetch("http://localhost:5000/tasks");
             const data = await res.json();
             return data;
-        }
+        },
     });
-
-    
-    // for delete
-    const handleDelete = task => {
-        fetch(`http://localhost:5000/tasks/${task._id}`, {
-            method: 'DELETE', 
-            headers: {
-                //authorization: `bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            if(data.deletedCount > 0){
-                refetch();
-                toast.success(`${task.name} deleted successfully`)
-            }
-        })
-    }
 
     //console.log("deddedeede", deleting);
 
@@ -44,24 +28,16 @@ const MyTask = () => {
     //     return <Loading></Loading>
     // }
 
-
     return (
-        <div className='mt-28 mx-10'>
-            
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                    {
-                        tasks.map(task => <TaskCard
-                            key={task._id}
-                            task={task}
-                            setDeleting={setDeleting}
-                            deleting={deleting}
-                            handleDelete={handleDelete}
-                        ></TaskCard>)
-                    }
+        <div className="mt-28 mx-10">
+            <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {tasks.map((task) => (
+                    <TaskCard key={task._id} task={task} refetch={refetch} />
+                ))}
             </div>
-            
         </div>
     );
 };
 
 export default MyTask;
+
--- src/Pages/MyTask/TaskCard.js
+++ src/Pages/MyTask/TaskCard.js
@@ -1,42 +1,69 @@
-import React from 'react';
-import ConfirmationModal from './ConfirmationModal';
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
 
-const TaskCard = ({task, setDeleting, deleting, handleDelete}) => {
-    const {name, TaskDetails, _id, image} = task;
-    console.log("TasK", task);
-    const closeModal = () => {
-        setDeleting(null);
+const TaskCard = ({ task, refetch }) => {
+    const { name, TaskDetails, _id, image } = task;
+
+    const [deleting, setDeleting] = useState(false);
+
+    // for delete
+    const deleteTask = () => {
+        fetch(`http://localhost:5000/tasks/${task._id}`, {
+            method: "DELETE",
+            headers: {
+                //authorization: `bearer ${localStorage.getItem('accessToken')}`
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.deletedCount > 0) {
+                    refetch();
+                    toast.success(`${task.name} deleted successfully`);
+                }
+            });
+    };
+
+    function handleDelete() {
+        console.log("hello");
+        confirmAlert({
+            title: "Confirm to submit",
+            message: "Are you sure to do this.",
+            buttons: [
+                {
+                    label: "Yes",
+                    onClick: () => deleteTask(),
+                },
+                {
+                    label: "No",
+                },
+            ],
+        });
     }
 
-    
     return (
         <div>
             <div class="max-w-sm rounded overflow-hidden shadow-lg">
                 <img class="w-full" src={image} alt="Sunset in the mountains" />
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">{name}</div>
-                    <p class="text-gray-700 text-base">
-                    {TaskDetails}
-                    </p>
+                    <p class="text-gray-700 text-base">{TaskDetails}</p>
                 </div>
                 <div class="px-6 py-4 grid gap-2 grid-flow-col">
-                    <button className='bg-indigo-600 hover:bg-indigo-400 mt-2 text-white font-bold py-2 px-2 rounded'>UPDATE</button>
-                    <button onClick={() => setDeleting(task)} htmlFor="confirmation-modal" className='bg-indigo-600 hover:bg-indigo-400 mt-2 text-white font-bold py-2 px-2 rounded'>DELETE</button>
-                    <button  className='bg-indigo-600 hover:bg-indigo-400 mt-2 text-white font-bold py-2 px-2 rounded'>COMPLEATE</button>
+                    <button className="bg-indigo-600 hover:bg-indigo-400 mt-2 text-white font-bold py-2 px-2 rounded">
+                        UPDATE
+                    </button>
+                    <button
+                        onClick={handleDelete}
+                        className="bg-indigo-600 hover:bg-indigo-400 mt-2 text-white font-bold py-2 px-2 rounded">
+                        DELETE
+                    </button>
+                    <button className="bg-indigo-600 hover:bg-indigo-400 mt-2 text-white font-bold py-2 px-2 rounded">
+                        COMPLETE
+                    </button>
                 </div>
-                
             </div>
-            {
-                    deleting && <ConfirmationModal
-                        title={` Are you sure you want to delete?`}
-                        message={`If you delete ${deleting.name}. It cannot be undone.`}
-                        successAction = {handleDelete}
-                        successButtonName="Delete"
-                        modalData = {deleting}
-                        closeModal = {closeModal}
-                    >
-                    </ConfirmationModal>
-                }
         </div>
     );
 };
